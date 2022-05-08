6a938156f43bc6fd8b1ad2b314a4f5e3e5560589
Matheus Bonato <matheusmbonato@gmail.com>
2022-05-08 04:13:42 -0300

    Adding splash screen

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:glp_manager_mobile/shared/themes/app_images.dart';
+import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Center(child: Image.asset(AppImages.logoFull, height: 200,)),
+    );
+  }
+}
