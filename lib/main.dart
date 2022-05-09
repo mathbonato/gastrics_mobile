82bd0a8b6f719e2c3728797a4ab22a6fe4744bb2
Matheus Bonato <matheusmbonato@gmail.com>
2022-05-08 21:41:43 -0300

    Adding lib for routing and state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:glp_manager_mobile/modules/home/home_page.dart';
-import 'package:glp_manager_mobile/modules/login/login_page.dart';
+import 'package:glp_manager_mobile/modules/home/home_page.dart';
 import 'package:glp_manager_mobile/modules/splash/splash_page.dart';
 import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(GetMaterialApp(
     theme: ThemeData(primaryColor: AppColors.primary),
-    home: const LoginPage(),
+    home: const HomePage(),
   ));
 }
