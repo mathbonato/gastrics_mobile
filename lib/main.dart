6e1e3e9e5db0bf64beaeee632a93e950ad2b1f64
Matheus Bonato <matheusmbonato@gmail.com>
2022-05-10 21:39:17 -0300

    adding login page

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:glp_manager_mobile/modules/home/home_page.dart';
 import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-      theme: ThemeData(primaryColor: AppColors.primary),
-      home: const HomePage(),
-    )
+  runApp(GetMaterialApp(home: HomePage())
   );
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -4,6 +4,11 @@ import 'package:glp_manager_mobile/shared/themes/app_text_styles.dart';
 import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
 import 'package:glp_manager_mobile/shared/widgets/social_login/social_login_button.dart';
 
+import '../../components/already_have_an_account_acheck.dart';
+import '../../components/rounded_button.dart';
+import '../../components/rounded_input_field.dart';
+import '../../components/rounded_password_field.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -16,14 +21,49 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(title: Text("GLP Manager"),),
-      body: Container(
-        width: size.width,
-        height: size.height,
-        color: AppColors.body,
-        child: Center(child: Text("Login Page")),
-      )
+        backgroundColor: AppColors.background,
+        appBar: AppBar(backgroundColor: AppColors.primary, title: Text("GLP Manager")),
+        body: Container(
+            width: double.infinity,
+            height: size.height,
+            child: Stack(
+                alignment: Alignment.center,
+                children: [
+                    SingleChildScrollView(
+                        child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                                Text(
+                                    "LOGIN",
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: size.height * 0.03),
+                                // SvgPicture.asset(
+                                //   "assets/icons/login.svg",
+                                //   height: size.height * 0.35,
+                                // ),
+                                SizedBox(height: size.height * 0.03),
+                                RoundedInputField(
+                                    hintText: "Your Email",
+                                    onChanged: (value) {},
+                                ),
+                                RoundedPasswordField(
+                                    onChanged: (value) {},
+                                ),
+                                RoundedButton(
+                                    text: "LOGIN",
+                                    press: () {},
+                                ),
+                                SizedBox(height: size.height * 0.03),
+                                AlreadyHaveAnAccountCheck(
+                                    press: () {},
+                                ),
+                            ],
+                        ),
+                    ),
+                ],
+            ),
+        ),
     );
   }
 }
