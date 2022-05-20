ba3d6979e38dce10388bc37dff5d5b4e5095273f
Matheus Bonato <matheusmbonato@gmail.com>
2022-05-19 23:40:02 -0300

    adding cards and grid view

--- lib/modules/dashboard/dashboard_page.dart
+++ lib/modules/dashboard/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:glp_manager_mobile/components/card.dart';
 import 'package:glp_manager_mobile/modules/dashboard/drawer.dart';
 import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
 
@@ -17,77 +18,80 @@ class _DashboardPageState extends State<DashboardPage> {
     String dropdownValue = 'Filial1';
 
     return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(
-          backgroundColor: AppColors.primary, title: const Text("GLP Manager")),
-      drawer: const CustomDrawer(),
-      body: Container(
-        width: double.infinity,
-        height: size.height,
-        child: SingleChildScrollView(
-            child: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(9.0),
-                child: DropdownButton<String>(
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_downward),
-                  elevation: 16,
-                  isExpanded: true,
-                  style: const TextStyle(color: AppColors.primary),
-                  underline: Container(
-                    height: 2,
-                    color: AppColors.primary,
-                  ),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropdownValue = newValue!;
-                    });
-                  },
-                  items: <String>['Filial1', 'Filial2', 'Filial3']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
+        backgroundColor: AppColors.background,
+        appBar: AppBar(
+            backgroundColor: AppColors.primary,
+            title: const Text("GLP Manager")),
+        drawer: const CustomDrawer(),
+        body: Column(
+          children: [
+            SizedBox(height: size.height * 0.01),
+            Padding(
+              padding: const EdgeInsets.all(9.0),
+              child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                isExpanded: true,
+                style: const TextStyle(color: AppColors.primary),
+                underline: Container(
+                  height: 2,
+                  color: AppColors.primary,
                 ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: <String>['Filial1', 'Filial2', 'Filial3']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               ),
-              SizedBox(height: size.height * 0.03),
-              Card(
-                  elevation: 0,
-                  shape: const RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: AppColors.grey,
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            Expanded(
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: const [
+                  CustomCard(
+                    title: "123 - teste",
+                    maxWeight: 40,
+                    currentWeight: 20,
+                    percentage: 50,
+                    date: '13/07/2022',
                   ),
-                  child: Container(
-                      height: 200,
-                      alignment: Alignment.topCenter,
-                      margin: const EdgeInsets.all(5),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0, left: 8.0),
-                            child: Row(
-                              children: const [
-                                CircularProgressIndicator(
-                                  backgroundColor: Colors.grey,
-                                  color: Colors.green,
-                                  strokeWidth: 10,
-                                  value: 0.65,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ))),
-            ],
-          ),
-        )),
-      ),
-    );
+                  CustomCard(
+                    title: "546 - teste1",
+                    maxWeight: 40,
+                    currentWeight: 30,
+                    percentage: 90,
+                    date: '13/07/2022',
+                  ),
+                  CustomCard(
+                    title: "466 - teste2",
+                    maxWeight: 40,
+                    currentWeight: 10,
+                    percentage: 100,
+                    date: '13/07/2022',
+                  ),
+                  CustomCard(
+                    title: "807 - teste3",
+                    maxWeight: 70,
+                    currentWeight: 10,
+                    percentage: 10,
+                    date: '13/02/2022',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
