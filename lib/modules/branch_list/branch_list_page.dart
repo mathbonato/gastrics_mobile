ba7e81fd8dfa86708cea97bded9b41030eb7beaf
GregoriSimei <gregorisimei@gmail.com>
2022-05-23 23:10:58 -0300

    Finished notification view

--- lib/modules/branch_list/branch_list_page.dart
+++ lib/modules/branch_list/branch_list_page.dart
@@ -4,6 +4,7 @@ import 'package:glp_manager_mobile/mock/BranchGenerator.dart';
 import 'package:glp_manager_mobile/models/Branch.dart';
 import 'package:glp_manager_mobile/modules/branch_create/branch_create_page.dart';
 import 'package:glp_manager_mobile/modules/dashboard/drawer.dart';
+import 'package:glp_manager_mobile/modules/notifications_view/notifications_view.dart';
 import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
 
 class BranchList extends StatefulWidget {
@@ -18,55 +19,65 @@ class _BranchListState extends State<BranchList> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
         backgroundColor: AppColors.background,
-        drawer: new CustomDrawer(),
+        drawer: const CustomDrawer(),
         appBar: AppBar(
           backgroundColor: AppColors.primary,
-          title: Center(child: Text("GLP Manager")),
+          title: const Center(child: Text("GLP Manager")),
           actions: <Widget>[
             Stack(children: [
               IconButton(
-                  onPressed: () {}, icon: Icon(Icons.notifications_none)),
+                onPressed: () {
+                  Get.to(NotificationsView());
+                },
+                icon: const Icon(Icons.notifications_none),
+              ),
               Container(
                 width: 7,
                 height: 7,
                 alignment: Alignment.center,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     color: Color.fromARGB(255, 221, 75, 209),
                     shape: BoxShape.circle),
-                margin: EdgeInsets.only(left: 27, top: 10),
+                margin: const EdgeInsets.only(
+                  left: 27,
+                  top: 10,
+                ),
               )
             ])
           ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Get.to(BranchCreate());
+            Get.to(const BranchCreate());
           },
           backgroundColor: AppColors.primary,
-          child: Icon(Icons.add),
+          child: const Icon(Icons.add),
         ),
         body: Container(
           child: Column(
             children: <Widget>[
               Container(
                 height: 80,
-                child: Center(
-                    child: Text(
-                  "Filiais",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Color.fromARGB(255, 85, 85, 85)),
-                )),
+                child: const Center(
+                  child: Text(
+                    "Filiais",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 85, 85, 85)),
+                  ),
+                ),
               ),
               Expanded(
                   child: Container(
                 child: ListView.separated(
-                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    bottom: 20,
+                  ),
                   itemCount: branches.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
@@ -74,24 +85,31 @@ class _BranchListState extends State<BranchList> {
                       child: Row(
                         children: <Widget>[
                           Container(
-                              child: CircleAvatar(
-                            backgroundColor: Colors.grey.shade200,
-                            backgroundImage: NetworkImage(branches[index].img),
-                          )),
-                          SizedBox(width: 20),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.grey.shade200,
+                              backgroundImage:
+                                  NetworkImage(branches[index].img),
+                            ),
+                          ),
+                          const SizedBox(width: 20),
                           Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  branches[index].name,
-                                  style: TextStyle(fontWeight: FontWeight.bold),
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                branches[index].name,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(
-                                  'R: ' + branches[index].street + ', nº 123',
-                                  style: TextStyle(color: Colors.grey[500]),
-                                )
-                              ])
+                              ),
+                              Text(
+                                'R: ' + branches[index].street + ', nº 123',
+                                style: TextStyle(
+                                  color: Colors.grey[500],
+                                ),
+                              )
+                            ],
+                          )
                         ],
                       ),
                     );
---
+++ lib/modules/notifications_view/notifications_view.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:glp_manager_mobile/components/notification_card.dart';
+import 'package:glp_manager_mobile/modules/dashboard/drawer.dart';
+import 'package:glp_manager_mobile/shared/themes/appcollors.dart';
+
+class NotificationsView extends StatefulWidget {
+  const NotificationsView({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationsView> createState() => _NotificationsViewState();
+}
+
+class _NotificationsViewState extends State<NotificationsView> {
+  @override
+  Widget build(BuildContext context) {
+    const alertTypes = [
+      'critical',
+      'warning',
+      'info',
+    ];
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      drawer: const CustomDrawer(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppColors.primary,
+        title: const Text("Notificações"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.separated(
+              padding: const EdgeInsets.all(10),
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return NotificationCard(
+                  title: 'Alerta - Troca de Gás',
+                  subtitle: 'Filial / Botijão da cozinha',
+                  indicatorVal: Random().nextInt(5),
+                  indicatorDescription: 'Dias',
+                  alertType: alertTypes[Random().nextInt(2)],
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 10);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
