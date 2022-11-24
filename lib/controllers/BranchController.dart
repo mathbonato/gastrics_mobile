87d579ac42b39099b91f6ba451118ace1877a384
GregoriSimei <gregorisimei@gmail.com>
2022-11-23 23:26:12 -0300

    Branch Controller

--- lib/controllers/BranchController.dart
+++ lib/controllers/BranchController.dart
@@ -8,6 +8,11 @@ class BranchController {
     Future<List<Branch>> getBranches(String companyId) async {
         Uri route = prepareUrl("/$companyId/branch");
         var response = await http.get(route);
+
+        if (response.statusCode != 200) {
+          return [];
+        }
+
         var json = jsonDecode(response.body);
 
         List<Branch> branches = [];
@@ -17,5 +22,48 @@ class BranchController {
         });
         
         return branches;
+    }
+
+    Future<Branch?> getBranch(String companyId, String branchId) async {
+      Uri route = prepareUrl("/$companyId/branch?id=$branchId");
+      var response = await http.get(route);
+
+      if (response.statusCode != 200) {
+        return null;
+      }
+
+      var json = jsonDecode(response.body); 
+
+      Branch newBranch = Branch.fromJson(json);
+
+      return newBranch;
+    }
+
+    Future<Branch?> postBranch(String companyId, Branch branch) async {
+      Uri route = prepareUrl("/$companyId/branch");
+      var response = await http.post(route, body: branch);
+
+      if (response.statusCode != 201) {
+        return null;
+      }
+
+      var json = jsonDecode(response.body); 
+
+      Branch newBranch = Branch.fromJson(json);
+
+      return newBranch;
+    }
+
+    Future<bool> deleteBranch(String companyId, Branch branch) async {
+      var branchId = branch.id;
+
+      Uri route = prepareUrl("/$companyId/branch?id=$branchId");
+      var response = await http.delete(route);
+
+      if (response.statusCode != 200) {
+        return false;
+      }
+
+      return true;
     }
 }
