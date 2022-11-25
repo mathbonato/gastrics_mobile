5a61b8c560581cd7f176866eb275ea806be92e08
GregoriSimei <gregorisimei@gmail.com>
2022-11-25 01:56:28 -0300

    Setting globals vars

--- lib/models/my-globals.dart
+++ lib/models/my-globals.dart
@@ -1,5 +1,12 @@
 library globals;
 
+import 'package:glp_manager_mobile/models/Company.dart';
+import 'package:glp_manager_mobile/models/Employee.dart';
 import 'package:glp_manager_mobile/models/LoginResponse.dart';
 
-LoginResponse? loginResponse; 
+LoginResponse? loginResponse;
+Company? company;
+String? currentBranchId;
+String? token;
+Employee? employee;
+String? employeeType;
